e87fb5161c55a12572ef2a1726cf20ee677d43e5
aruna02 <aroona.shrestha@gmail.com>
2017-02-19 20:14:01 +0545

    some other example

---
+++ Area.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+class Area{
+
+    void area(int r)
+    //area of circle
+    {
+       double a=2*3.14*r;
+	System.out.println("area of circle is :"+a);
+    }
+
+    void area(int l, int b)
+    {
+	int a=2*(l+b);
+	System.out.println("area of rectangle is :"+a);
+    }
+    public static void main(String args[])
+    {
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter r=");
+		int a = scan.nextInt();
+		//	System.out.println("you have entered radius: "+a);
+	System.out.println("enter value of length=");
+	
+	int l=scan.nextInt();
+
+	System.out.println("Enter breadth=");
+	int b=scan.nextInt();
+	Area areaa= new Area();
+	areaa.area(a);
+	areaa.area(l,b);
+
+	
+	}
+
+
+}
